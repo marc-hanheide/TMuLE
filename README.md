2bff85d4d4e3c4af73d95ef94d60b542ea66fdf4
Marc Hanheide <marc@hanheide.net>
2017-07-28 03:05:37 +0100

    added all files for first release

--- README.md
+++ README.md
@@ -1,2 +1,122 @@
-# TMuLE
-The TMux Launch Engine
+# TMuLE - TMux Launch Engine
+
+## What is TMuLE for?
+
+To simply control [tmux](https://github.com/tmux/tmux/wiki) sessions to deploy pre-configured software system very easily. All it needs is a configuration file (in JSON) and different sub-systems (defined as different windows in a joint tmux sessions) can be launch and stopped conveniently.
+
+## Concepts
+
+[tmux](https://github.com/tmux/tmux/wiki) features *windows* which can have different *panes*. In *TMuLE* windows correspond to sub-systems that can be launch and stopped independently of others, but always are launched or stopped together. Each pane in a window runs one of a set of commands belonging to one sub-system.
+
+## Configuration File
+
+The configuration file uses JSON format.
+
+* an `init_cmd` can be defined which is executed in each *pane before* the actual command is run.
+* the `windows` are a list of individual windows that will be created in the tmux session. Each corresponds to one sub-system. 
+* Each window is given a `name`, and a list of `panes`
+* the entries in the `panes` list, are shell scripts commands that are executed *as is* in the tmux session shell (i.e. *bash*)
+
+Here is an example `sample.json`:   
+
+```json
+{
+  "init_cmd": "source ~/spqrel/spqrel_launch/setup.bash; source ~/spqrel/spqrel_launch/worktree/spqrel_tools/setup-dev.bash /opt/naoqi; sleep 3",
+  "windows": [
+    {
+      "name": "bash",
+      "panes": [
+        "bash"
+      ]
+    },
+    {
+      "name": "htop",
+      "panes": [
+        "htop"
+      ]
+    },
+    {
+      "name": "navigation",
+      "panes": [
+        "pepper_localizer",
+        "pepper_planner"
+      ]
+    }
+  ]
+}
+```
+
+## Usage
+
+Just run `tmux.py -h`, output whould be something like this:
+
+
+```
+usage: tmux.py [-h] [--config CONFIG] [--init INIT] [--session SESSION]
+               {list,launch,stop,relaunch,terminate,server,pids,running} ...
+
+positional arguments:
+  {list,launch,stop,relaunch,terminate,server,pids,running}
+                        sub-command help
+    list                show windows
+    launch              launch window(s)
+    stop                stop windows(s)
+    relaunch            relaunch windows(s)
+    terminate           kill window(s)
+    server              run web server
+    pids                pids of processes
+    running             returns true of there is a process running in the
+                        window
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --config CONFIG       JSON config file. see sample-config.json. Default:
+                        spqrel-pepper-config.json
+  --init INIT           Should tmux be initialised? Default: True
+  --session SESSION     The session that is controlled. Default: spqrel```
+
+### Typical usage (corresponding to the `sample.json` file above)
+
+Note: If you are using the default config file, you can obviously skip using the `--config` option below.
+
+* list the windows in the tmux session:
+
+  `tmux --config sample.json list`
+
+  If the `init` option is not explicitly set to `False` this will create (or attach to) the default (`spqrel`) tmux session, and will make sure that all the windows that are configured and required panes are created. 
+
+* launch one specific window (sub-system):
+
+  `tmux -config sample.json launch -w navigation`
+
+  This will launch all the configured commands in the `navigation` window in their two respective panes.
+
+* stop the processes of a window (sub-system):
+
+	`tmux -config sample.json stop -w navigation`
+
+	This command will send `Ctrl-C` to all the panes of the `navigation` window, and hence stop all the processes in there. 
+
+* kill the processes of a window (sub-system):
+
+	`tmux -config sample.json kill -w navigation`
+
+	This command will send `Ctrl-C` to all the panes of the `navigation` window just like `stop` would do, but follows this with a proper `kill -9` for all child processes to make sure everything is sure and properly gone. It will also close the respective window in the session. 
+
+* if in any of the above commands, now window is specified (no `-w` or `--window` option given), the command will apply to *ALL* configured windows (sub-systems), e.g.
+
+	`tmux -config sample.json launch`
+
+	will launch all processes
+
+	`tmux -config sample.json kill`
+
+	will shut everything down and in fact close the tmux session
+
+* Manual interaction with the tmux session:
+
+	`tmux a -t sqprel`
+
+	This will attach to the default TMuLE session, allwoing manual inspection and interaction with the session (e.g. watching the output of the individual processes, or even starting and stopping them manually)
+
+	Use `Ctrl-b d` to detach from the session again, or `Ctrl-b w` to switch between different windows. 
---
+++ tmux.py
@@ -0,0 +1,376 @@
+#! /usr/bin/env python
+from libtmux import Server
+from json import load
+from logging import error, warn, info, debug, basicConfig, INFO, WARN
+from pprint import pformat, pprint
+from time import sleep
+import signal
+import os
+from os import path
+import argparse
+from psutil import Process, wait_procs
+import sys
+
+from datetime import datetime
+basicConfig(level=INFO)
+
+sys.path.append(os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..'))
+)
+
+
+class TMux:
+
+    def __init__(self, session_name="spqrel", configfile=None):
+        if configfile:
+            self.load_config(configfile)
+        else:
+            self.config = None
+        self.session_name = session_name
+
+    def _on_terminate(self, proc):
+        info("process {} terminated with exit code {}"
+             .format(proc, proc.returncode))
+
+    def _terminate(self, pid):
+        procs = Process(pid).children()
+        for p in procs:
+            p.terminate()
+        gone, still_alive = wait_procs(procs, timeout=1,
+                                       callback=self._on_terminate)
+        for p in still_alive:
+            p.kill()
+
+    def _get_children_pids(self, pid):
+        return Process(pid).children(recursive=True)
+
+    def load_config(self, filename="sample_config.json"):
+        with open(filename) as data_file:
+            self.config = load(data_file)
+
+    def init(self):
+        if not self.config:
+            error('config file not loaded; call "load_config" first!')
+        else:
+            self.server = Server()
+            if self.server.has_session(self.session_name):
+                self.session = self.server.find_where({
+                    "session_name": self.session_name
+                })
+
+                info('found running session %s on server' % self.session_name)
+            else:
+                info('starting new session %s on server' % self.session_name)
+                self.session = self.server.new_session(
+                    session_name=self.session_name
+                )
+
+            for win in self.config['windows']:
+                window = self.session.find_where({
+                    "window_name": win['name']
+                })
+                if window:
+                    debug('window %s already exists' % win['name'])
+                else:
+                    debug('create window %s' % win['name'])
+                    window = self.session.new_window(win['name'])
+                exist_num_panes = len(window.list_panes())
+                while exist_num_panes < len(win['panes']):
+                    debug('new pane needed in window %s' % win['name'])
+                    window.split_window(vertical=1)
+                    exist_num_panes = len(window.list_panes())
+                window.cmd('select-layout', 'tiled')
+
+    def find_window(self, window_name):
+        for win in self.config['windows']:
+            if win['name'] == window_name:
+                window = self.session.find_where({
+                    "window_name": win['name']
+                })
+                window.select_window()
+                return win, window
+
+    def send_ctrlc(self, pane):
+        datestr = datetime.now().strftime('%c')
+        pane.cmd("send-keys", "", "C-c")
+        pane.cmd("send-keys", "", "C-c")
+        pane.cmd("send-keys", "", "C-c")
+        pane.send_keys('# tmux-controller sent Ctrl-C at %s' % datestr,
+                       enter=True, suppress_history=True)
+
+    def launch_window(self, window_name, enter=True):
+        info('launch %s' % window_name)
+        winconf, window = self.find_window(window_name)
+        pane_no = 0
+        datestr = datetime.now().strftime('%c')
+        for cmd in winconf['panes']:
+            pane = window.select_pane(pane_no)
+            self.send_ctrlc(pane)
+            pane.send_keys('# tmux-controller starts new command %s' % datestr,
+                           enter=True, suppress_history=True)
+            if 'init_cmd' in self.config:
+                pane.send_keys(self.config['init_cmd'],
+                               enter=enter, suppress_history=False)
+            pane.send_keys(cmd, enter=enter, suppress_history=False)
+            pane_no += 1
+        winconf['_running'] = True
+
+    def launch_all_windows(self):
+        for winconf in self.config['windows']:
+            self.launch_window(winconf['name'])
+
+    def stop_all_windows(self):
+        for winconf in self.config['windows']:
+            self.stop_window(winconf['name'])
+
+    def get_children_pids_all_windows(self):
+        pids = []
+        for winconf in self.config['windows']:
+            pids.extend(
+                self.get_children_pids_window(winconf['name'])
+            )
+        return pids
+
+    def kill_all_windows(self):
+        for winconf in self.config['windows']:
+            self.kill_window(winconf['name'])
+
+    def stop_window(self, window_name):
+        info('stop %s' % window_name)
+        winconf, window = self.find_window(window_name)
+        self._stop_window(winconf, window)
+
+    def _stop_window(self, winconf, window):
+        pane_no = 0
+        for cmd in winconf['panes']:
+            pane = window.select_pane(pane_no)
+            self.send_ctrlc(pane)
+            pane_no += 1
+        pids = self._get_pids_window(window)
+        sleep(.1)
+        for p in pids:
+            self._terminate(p)
+        winconf['_running'] = False
+
+    def kill_window(self, window_name):
+        info('terminate %s' % window_name)
+        winconf, window = self.find_window(window_name)
+#                       "-F '#{pane_active} #{pane_pid}")
+        self._stop_window(winconf, window)
+        pids = self._get_pids_window(window)
+        for pid in pids:
+            Process(pid).terminate()
+        winconf['_running'] = False
+
+    def list_windows(self):
+        return [w['name'] for w in self.config['windows']]
+
+    def get_pids_window(self, window_name):
+        winconf, window = self.find_window(window_name)
+        return self._get_pids_window(window)
+
+    def _get_pids_window(self, window):
+        r = window.cmd('list-panes',
+                       "-F #{pane_pid}")
+        return [int(p) for p in r.stdout]
+
+    def get_children_pids_window(self, window_name):
+        winconf, window = self.find_window(window_name)
+        return self._get_children_pids_window(window)
+
+    def _get_children_pids_window(self, window):
+        winpids = self._get_pids_window(window)
+        pids = []
+        for pid in winpids:
+            pids.extend(self._get_children_pids(pid))
+        return [p.pid for p in pids]
+
+    def is_running(self, window_name):
+        winconf, window = self.find_window(window_name)
+        pids = self._get_children_pids_window(window)
+        return len(pids) > 0
+
+    def _server(self):
+        from ui import webnsock
+        import web
+
+        tmux_self = self
+
+        class TMuxWebServer(webnsock.ControlServer):
+
+            def __init__(self):
+
+                webnsock.ControlServer.__init__(self)
+
+                TEMPLATE_DIR = path.realpath(
+                    path.join(
+                        path.dirname(__file__),
+                        'www'
+                    )
+                )
+                print TEMPLATE_DIR, __file__
+
+                os.chdir(TEMPLATE_DIR)
+
+                render = web.template.render(TEMPLATE_DIR,
+                                             base='base', globals=globals())
+
+                class Index(self.page):
+                    path = '/'
+
+                    def GET(self):
+                        return render.index(tmux_self.config)
+
+        class TMuxWSProtocol(webnsock.JsonWSProtocol):
+
+            def __init__(self):
+                super(TMuxWSProtocol, self).__init__()
+
+            def on_button(self, payload):
+                info('button pressed: \n%s' % pformat(payload))
+                window_name = payload['id']
+                cmd = payload['cmd']
+                if cmd == 'launch':
+                    if window_name == '':
+                        tmux_self.launch_all_windows()
+                    else:
+                        tmux_self.launch_window(window_name)
+                elif cmd == 'stop':
+                    if window_name == '':
+                        tmux_self.stop_all_windows()
+                    else:
+                        tmux_self.stop_window(window_name)
+                elif cmd == 'terminate':
+                        tmux_self.kill_all_windows()
+                        sleep(1)
+                        tmux_self.init()
+
+                sleep(1)
+                self.sendJSON(self.on_status())
+
+            def on_status(self, payload=None):
+                info('status-requested: ')
+
+                res = {
+                    'windows': {},
+                    'method': 'update_status'
+                }
+                for w in tmux_self.config['windows']:
+                    res['windows'][w['name']] = tmux_self.is_running(w['name'])
+
+                return res
+
+                #return {'button_outcome': True}
+
+        self.webserver = webnsock.Webserver(TMuxWebServer(), port=9999)
+        self.backend = webnsock.WSBackend(TMuxWSProtocol)
+
+        signal.signal(
+            signal.SIGINT,
+            lambda s, f: webnsock.signal_handler(
+                self.webserver, self.backend, s, f))
+        self.webserver.start()
+        self.backend.talker(port=9998)
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str,
+                        default='sample_config.json',
+                        help="JSON config file. see sample-config.json. Default: spqrel-pepper-config.json")
+    parser.add_argument("--init", type=bool, default=True,
+                        help="Should tmux be initialised? Default: True")
+    parser.add_argument("--session", type=str,
+                        default='tmule',
+                        help="The session that is controlled. Default: spqrel")
+
+    subparsers = parser.add_subparsers(dest='cmd',
+                                       help='sub-command help')
+    parser_list = subparsers.add_parser('list', help='show windows')
+    parser_launch = subparsers.add_parser('launch', help='launch window(s)')
+    parser_launch.add_argument("--window", '-w', type=str,
+                               default="",
+                               help="Window to be launched. Default: ALL")
+    parser_stop = subparsers.add_parser('stop', help='stop windows(s)')
+    parser_stop.add_argument("--window", '-w', type=str,
+                             default="",
+                             help="Window to be stopped. Default: ALL")
+    parser_relaunch = subparsers.add_parser('relaunch',
+                                            help='relaunch windows(s)')
+    parser_relaunch.add_argument("--window", '-w', type=str,
+                                 default="",
+                                 help="Window to be relaunched. Default: ALL")
+    parser_kill = subparsers.add_parser('terminate', help='kill window(s)')
+    parser_kill.add_argument("--window", '-w', type=str,
+                             default="",
+                             help="Window to be killed. Default: ALL")
+    parser_server = subparsers.add_parser('server', help='run web server')
+    parser_pids = subparsers.add_parser('pids', help='pids of processes')
+    parser_pids.add_argument(
+        "--window", '-w', type=str,
+        default="",
+        help="Window for which PIDs are shown. Default: ALL")
+    parser_pids = subparsers.add_parser(
+        'running',
+        help='returns true of there is a process running in the window')
+    parser_pids.add_argument("--window", '-w', type=str,
+                             required=True,
+                             help="Window to be checked.")
+
+    args = parser.parse_args()
+
+    tmux = TMux(configfile=args.config)
+
+    if (args.init):
+        tmux.init()
+
+    if args.cmd == 'list':
+        print(pformat(tmux.list_windows()))
+    elif args.cmd == 'launch':
+        if args.window == '':
+            tmux.launch_all_windows()
+            pass
+        else:
+            tmux.launch_window(args.window)
+    elif args.cmd == 'stop':
+        if args.window == '':
+            tmux.stop_all_windows()
+        else:
+            tmux.stop_window(args.window)
+    elif args.cmd == 'relaunch':
+        if args.window == '':
+            tmux.stop_all_windows()
+            sleep(1)
+            tmux.launch_all_windows()
+        else:
+            tmux.stop_window(args.window)
+            sleep(1)
+            tmux.launch_window(args.window)
+    elif args.cmd == 'terminate':
+        if args.window == '':
+            tmux.kill_all_windows()
+        else:
+            tmux.kill_window(args.window)
+    elif args.cmd == 'running':
+        print tmux.is_running(args.window)
+    elif args.cmd == 'server':
+        tmux._server()
+    elif args.cmd == 'pids':
+        if args.window == '':
+            print(pformat(tmux.get_children_pids_all_windows()))
+        else:
+            print(pformat(tmux.get_children_pids_window(args.window)))
+    else:
+        error('unknown command %s', args.cmd)
+
+    # windows_to_launch = [
+    #     'htop', 'navigation', 'speech', 'ui', 'pnp', 'dataset'
+    # ]
+    # for w in windows_to_launch:
+    #     tmux.launch_window(w, True)
+    #     sleep(1)
+    # #sleep(8)
+    # tmux.stop_all_windows()
+    # tmux.terminate()
